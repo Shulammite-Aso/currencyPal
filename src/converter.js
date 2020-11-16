191ba7a9c3fdffe7ee61b0bc9e5acd8fd661d33a
Shulammite-Aso <asoamarachi@gmail.com>
2020-11-16 21:38:18 +0000

    initial commit

---
+++ src/converter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Converter() {
+    return (
+        <form>
+            <input id="from" type="text"></input>
+            <input id="to" type="text"></input>
+        </form>
+    );
+};
